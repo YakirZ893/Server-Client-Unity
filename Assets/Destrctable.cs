2150c4bebe7aacbd508d3f51283239b64e58bbd3
YakirZ893 <60983683+YakirZ893@users.noreply.github.com>
2021-07-04 22:41:28 +0300

    Added destructible obstacle

---
+++ Assets/Destrctable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Destrctable : NetworkBehaviour
+{
+    public GameObject destroyedObject;
+    public ServerData serverData;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == serverData.GetPlayer())
+        {
+            Instantiate(destroyedObject, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        
+
+        
+    }
+
+
+}
